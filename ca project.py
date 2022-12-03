6610645991f40c1af6c556fbec44cb48f188dcc3
r-AjivnAyan <119072341+r-AjivnAyan@users.noreply.github.com>
2022-12-04 01:59:45 +0530

    Update ca project.py

--- ca project.py
+++ ca project.py
@@ -2,12 +2,12 @@ import random
 
 name=input("Enter your name: ")
 resd = int(input("Enter your Registration number: "))
-Q1 = "Sharks are mammals."
-A1 = "False"
+Q1 = "Coffee is made from berries"
+A1 = "True"
 #False: Sharks are fishes.
-Q2 = "Bats are blind."
-A2 = "False"
-#False: Bats use ultrasonic waves to find their direction, but the are not blind.
+Q2 = "Alliumphobia is a fear of garlic."
+A2 = "True"
+#true: .
 Q3 = "An ant can lift 1000 times its body weight."
 A3 = "False"
 #False: they can lift upto 5000 times their weight.
@@ -23,9 +23,9 @@ A6 = "False"
 Q7 = "Vatican city is the smallest country in the World."
 A7 = "True"
 #True: measuring a mere 0.2 square miles, it is almost 120 times smaller than Manhattan in New York.
-Q8 = "There are 300 bones in an average adult human."
-A8 = "False"
-#False: A human baby has 300 bones whereas human adult has 206.
+Q8 = "The blue whale is the biggest animal to have ever lived."
+A8 = "True"
+#False: that includes dinosaurs!.
 Q9 = "Heart is the largest internal organ in the human body."
 A9 = "False"
 #False: It is Liver.
@@ -35,18 +35,18 @@ A10 = "True"
 Q11 = "French fries originated from France."
 A11 = "False"
 #False: Thanks to Germany, we have french fries.
-Q12 = "Christopher Columbus found America from India."
+Q12 = "The Harry Potter film, Chamber of Secrets, is the longest film of the Harry Potter movies."
 A12 = "True"
-#True: He came to India in 3 August,1492.
-Q13 = "George Washington is the 1st President of America."
+#True:  set aside 161 minutes to watch this movie!.
+Q13 = "Brazil is the only nation to have played in every World Cup finals tournament."
 A13 = "True"
-#True: He was the 1st and John Adams is the 2nd.
+#True: the soccer-crazed country has also won the most titles..
 Q14 = "The maximum length for a video posted on TikTok is 45 seconds."
 A14 = "False"
 #False: No, it is 60 seconds.
-Q15 = "People who are afraid of crowd are called Cynophobic."
-A15 = "False"
-#False: They are called Agorophobic. Cynophobia is the fear of dogs.
+Q15 = "Marvel published its first comic in October 1939."
+A15 = "True"
+#true:if you can get your hands on a copy, you could sell it for up to $1 million..
 dict = { Q1 : A1, Q2 : A2, Q3: A3, Q4: A4, Q5 : A5, Q6 : A6,Q7 : A7, Q8 : A8, Q9 :
 A9, Q10 : A10, Q11 : A11, Q12: A12, Q13: A13, Q14 : A14, Q15 : A15}
 score = 0
